0799b1c4530408a9424ce8d4e5ca1b027a11a596
sansoune <yassine.dakhlaoui06@gmail.com>
2025-10-24 22:10:30 +0000

    connecting to dock client

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
-	"os"
+	// "os"
 
 	"cloudhub/internal/docker"
 )
 
 func main()  {
-	fmt.Println("hello from cloudhub")
-	fmt.Println(os.Args)
+	fmt.Println("🏠 Cloudhub - Docker Monitor")
+	fmt.Println("Attempting to connect to Docker...\n")
+
+	client, err := docker.NewClient()
+
+	if err != nil {
+		fmt.Printf("❌ Error: %v\n", err)
+		fmt.Println("💡 Make sure Docker is running!")
+		return
+	}
+	defer client.Close()
+
+	fmt.Println("✅ Successfully connected to Docker!")
 }
--- internal/docker/client.go
+++ internal/docker/client.go
@@ -1,13 +1,29 @@
 package docker
 
-
 import (
-	"context"
 	"fmt"
-	"strings"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
-
 )
+
+type Client struct {
+	cli *client.Client
+}
+
+// creating a new docker client
+func NewClient() (*Client, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to docker: %w", err)
+	}
+
+	return &Client{
+		cli: cli,
+	}, nil
+
+}
+
+//close client connection
+func (c *Client) Close() error {
+	return c.cli.Close()
+}
